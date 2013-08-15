ccf46ce2cf1ce639398468ff7838243788eb37af
Ilan Peer <ilan.peer@intel.com>
2013-08-15 15:51:27 +0300

    WCD-AOSP: Merge diff from AOSP
    Merged diff between 88c8bf311efa4eb64468bed9b41b3110ab9effff and
    android-4.3_r2.1.
    
    Note: The changes in driver_nl80211 around nesting attributes were
    not merged.
    
    Change-Id: I0a4b8e3b616d794bd43c0fe9e369624e6034e552
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    
    x-iwlwifi-hostap-dev-build: 6dff228c1ce5a1810998e737fe60442f9661cea9

---
+++ Android.mk
@@ -0,0 +1,3 @@
+ifeq ($(WPA_SUPPLICANT_VERSION),VER_2_1_DEVEL_WCS)
+    include $(call all-subdir-makefiles)
+endif
---
+++ src/common/version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#ifndef VERSION_STR_POSTFIX
+#define VERSION_STR_POSTFIX ""
+#endif /* VERSION_STR_POSTFIX */
+
+
+#define VERSION_STR "2.0-devel-MWG" VERSION_STR_POSTFIX
+
+#endif /* VERSION_H */
