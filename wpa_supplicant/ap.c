b8f67e49d405cdbc5960347b2f85b8fd5645e238
Andrei Otcheretianski <andrei.otcheretianski@intel.com>
2013-11-13 05:49:02 +0200

    wpa_supplicant: Reuse csa_settings parsing code
    The wpa_supplicant's and hostapd's control interface implementation
    of chan_switch command duplicates csa_settings parsing code.
    Reuse the code by moving the parsing to ctrl_iface_ap.c
    
    Change-Id: Iee76fa40362276dd15be444b787611dc221193c8
    Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/25782
    Tested-by: IWL Jenkins
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    
    x-iwlwifi-hostap-dev-build: e4124074dfec11cf0cbf9000f74689f941406f8d

--- wpa_supplicant/ap.c
+++ wpa_supplicant/ap.c
@@ -1177,39 +1177,10 @@ int ap_ctrl_iface_chanswitch(struct wpa_supplicant *wpa_s,
 			     const char *pos)
 {
 	struct csa_settings settings;
-	char *end;
+	int ret = hostapd_parse_csa_settings(pos, &settings);
 
-	os_memset(&settings, 0, sizeof(settings));
-	settings.cs_count = strtol(pos, &end, 10);
-	if (pos == end) {
-		wpa_printf(MSG_ERROR, "chanswitch: invalid cs_count provided");
-		return -1;
-	}
-
-	settings.freq_params.freq = atoi(end);
-	if (settings.freq_params.freq == 0) {
-		wpa_printf(MSG_ERROR, "chanswitch: invalid freq provided");
-		return -1;
-	}
-
-#define SET_CSA_SETTING(str) \
-	do { \
-		const char *pos2 = os_strstr(pos, " " #str "="); \
-		if (pos2) { \
-			pos2 += sizeof(" " #str "=") - 1; \
-			settings.freq_params.str = atoi(pos2); \
-		} \
-	} while (0)
-
-	SET_CSA_SETTING(center_freq1);
-	SET_CSA_SETTING(center_freq2);
-	SET_CSA_SETTING(bandwidth);
-	SET_CSA_SETTING(sec_channel_offset);
-	settings.freq_params.ht_enabled = !!os_strstr(pos, " ht");
-	settings.freq_params.vht_enabled = !!os_strstr(pos, " vht");
-	settings.block_tx = !!os_strstr(pos, " blocktx");
-
-#undef SET_CSA_SETTING
+	if (ret)
+		return ret;
 
 	return ap_switch_channel(wpa_s, &settings);
 }
